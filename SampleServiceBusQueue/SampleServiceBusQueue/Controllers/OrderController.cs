a4c4d2d90689772ba35d2a2b00b7bc5db98ce1b0
chetan1987chd@gmail.com <chetan1987chd@gmail.com>
2023-01-23 18:05:09 +0530

    Initial Commit

---
+++ SampleServiceBusQueue/SampleServiceBusQueue/Controllers/OrderController.cs
@@ -0,0 +1,58 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SampleServiceBusQueue.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SampleServiceBusQueue.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+
+        string _connectionString = "<replace your connection string>";
+        string _queueName = "orderqueue";
+
+
+        public OrderController()
+        {
+        }
+
+        [HttpGet]
+        [Route("CreateOrder")]
+        public async Task CreateOrder()
+        {
+           //creates the client
+            ServiceBusClient queueClient = new ServiceBusClient(_connectionString);
+
+            //prepares the sender
+            ServiceBusSender queueSender = queueClient.CreateSender(_queueName);
+
+            OrderModel orderDetails = new OrderModel()
+            {
+                OrderId = 12,
+                Price = 4500,
+                Quantity = 2,
+                ProductId = 7
+            };
+
+            //stringify the object to json
+            string jsonString = JsonSerializer.Serialize(orderDetails);
+
+            //creates the message
+            ServiceBusMessage message = new ServiceBusMessage(jsonString);
+
+            //sends the message to queue
+            await queueSender.SendMessageAsync(message);
+
+
+
+
+        }
+    }
+}
---
+++ SampleServiceBusQueue/SampleServiceBusQueue/Models/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleServiceBusQueue.Models
+{
+    public class OrderModel
+    {
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
